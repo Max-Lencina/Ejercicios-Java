fe2f769186fb11a20a338b063ebe3f435010d18e
Max-Lencina <maxilencina1985@gmail.com>
2022-11-25 21:53:18 -0300

    segundo commit

--- ejemplopoo/src/ejemplopoo/Auto.java
+++ ejemplopoo/src/ejemplopoo/Auto.java
@@ -10,17 +10,36 @@ public class Auto {
     String color;
     boolean caja_manual;
     
+    //Contructores
+
+    public Auto() {
+    }
+
+    public Auto(int id, String patente, int can_puertas, String color, boolean caja_manual) {
+        this.id = id;
+        this.patente = patente;
+        this.can_puertas = can_puertas;
+        this.color = color;
+        this.caja_manual = caja_manual;
+    }
+    
+    
+    
+    
+    
+    
+    
     //Metodos
     public void encender(){
-        System.out.println("Estoy encendiendo");
+        System.out.println("Estoy encendiendo ");
     } 
     public void apagar(){
-        System.out.println("Estoy apagando");
+        System.out.println("Estoy apagando ");
     } 
     public void acelerar(double km){
-        System.out.println("Estoy acelerando" + km + "kms");
+        System.out.println("Estoy acelerando " + km + "kms");
     } 
     public void frenar(double km){
-        System.out.println("Estoy frenando" + km + "kms");
+        System.out.println("Estoy frenando " + km + "kms");
     }
 }
--- ejemplopoo/src/ejemplopoo/ejemplopoo.java
+++ ejemplopoo/src/ejemplopoo/ejemplopoo.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejemplopoo;
 
-/**
- *
- * @author Usuario
- */
+
 public class ejemplopoo {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+     
+        Auto auto1 = new Auto();
+        Auto auto2 = new Auto(1, "AA111JO", 5, "rojo", true);  
+        
+       auto2.encender();
+       auto2.acelerar(60);
+       auto2.frenar(30);
+       auto2.apagar();
     }
     
 }
